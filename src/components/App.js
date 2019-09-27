a4c26882cf82905ef557602152a4f6684c308187
nnacin <natali@thebyte9.com>
2019-09-28 00:41:46 +0200

    fixed import

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Country from './Country';
 import Home from './Home';
 import Info from './Info';
-import Tags from '../Tags';
+import Tags from './Tags';
 import '../App.css';
 
 function App() {
