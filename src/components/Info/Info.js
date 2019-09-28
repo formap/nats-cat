7f6c62ee7e9e3265336babe65dd5ce6e1187079e
nnacin <natali@thebyte9.com>
2019-09-28 08:51:53 +0200

    silly changes

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import './info.css';
+
 
 function Info() {
   return (
-    <div>nat info</div>
+    <>
+      <div className='infoBox'>
+        <ul>
+          <li>Area: 56,594 km2</li>
+          <li>Population: 4,076,246</li>
+          <li>Capital: Zagreb</li>
+          <li>Currency: Kuna (HRK)</li>
+          <li>Date format: dd.mm.yyyy (CE)</li>
+          <li>Driving side: right</li>
+          <li>Calling code:	+385</li>
+          <li>Internet TLD:	.hr</li>
+        </ul>
+      </div>
+      <div className='funFactBox'>
+      
+      </div>
+    </>
   );
 }
 
