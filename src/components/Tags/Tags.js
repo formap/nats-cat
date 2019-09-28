0529260bd2c97e684e0a158d46d6236862b5502e
nnacin <natali@thebyte9.com>
2019-09-28 12:06:02 +0200

    added popular tags from the api

--- src/components/Tags/Tags.js
+++ src/components/Tags/Tags.js
@@ -3,26 +3,28 @@ import Tag from '../Tag';
 import './tags.css';
 
 class Tags extends React.Component {
+  state = {
+    tags: []
+  };
+
+  componentDidMount() {
+    // this.loadMap();
+    this.getTags()
+  }
+
+  getTags() {
+    fetch(`http://10.10.72.89:8080/articlesTagged?country=${this.props.match.params.country}`)
+      .then((data) => data.json())
+      .then((res) => {
+        const tagsApi = [];
+        for (let [key, ] of Object.entries(res)) {
+          tagsApi.push(key);
+          this.setState({ tags: tagsApi });
+        }
+    });
+  }
   render() {
-    const tag = [
-      {name: 'politics', color: 'purple'}, 
-      {name: 'culture', color: 'teal'}, 
-      {name: 'environment', color: 'light-green'},
-      {name: 'sports', color: 'light-blue'},
-      {name: 'opinion', color: 'calm-red'}
-    ];
-    const tags = [];
-
-    for (let i = 0; i < tag.length; ++i) {
-      let name = tag[i].name;
-      if (!!name) {
-        name = name.toLowerCase();
-        name = name.charAt(0).toUpperCase() + name.substring(1);
-      }
-      let color = tag[i].color;
-      tags.push(<Tag key={name} country={this.props.match.params.country} tag={tag[i].name} name={name} color={color}/>);
-    }
-
+    const colors = ['purple', 'teal', 'light-green', 'light-blue', 'calm-red', 'yellow']
     return (
       <div className='tags-wrapper'>
         <div className='header'>
@@ -31,7 +33,7 @@ class Tags extends React.Component {
           </div>
           <div className='section-header'>Popular tags</div>
         </div>
-        {tags}
+        {this.state.tags.map((tag, key) => <Tag key={tag} country={this.props.match.params.country} tag={tag} name={tag} color={colors[key]}/>)}
       </div>
     );
   }
