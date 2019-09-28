5453f459bf6c4c4a13282fbd5c784d073c146384
Florencia Tarditti <maria.florencia.tarditti@est.fib.upc.edu>
2019-09-28 12:26:26 +0200

    add information page with layout

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -2,26 +2,81 @@ import React from 'react';
 import './info.css';
 
 
-function Info() {
-  return (
-    <>
-      <div className='infoBox'>
-        <ul>
-          <li>Area: 56,594 km2</li>
-          <li>Population: 4,076,246</li>
-          <li>Capital: Zagreb</li>
-          <li>Currency: Kuna (HRK)</li>
-          <li>Date format: dd.mm.yyyy (CE)</li>
-          <li>Driving side: right</li>
-          <li>Calling code:	+385</li>
-          <li>Internet TLD:	.hr</li>
-        </ul>
-      </div>
-      <div className='funFactBox'>
-      
-      </div>
-    </>
-  );
+class Info extends React.Component {
+  render() {
+    return (
+        <div className='infoBox'>
+          <div className='header'>
+            <div className='icon left' onClick={() => this.props.history.goBack()}>
+              <img src='/icons/back.svg' alt='Back Icon' />
+            </div>
+            <div className='section-header title'>About {this.props.countryName}</div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Population</span>
+              <span className='value'>4,076,246{this.props.population}</span>
+            </div>
+            <div className='information-field right'>
+              <span className='label'>Area</span>
+              <span className='value'>56,594 km2{this.props.area}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Capital</span>
+              <span className='value'>Zagreb{this.props.capital}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Currency</span>
+              <span className='value'>Kuna (HRK){this.props.currency}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='value'>1 EUR{this.props.currency0}</span>
+            </div>
+            <div className='information-field right'>
+              <span className='value'>GBP{this.props.currency1}</span>
+              <span className='value'>USD{this.props.currency2}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Government</span>
+              <span className='value'>Pedro Sánchez{this.props.leader}</span>
+              <span className='value'>Socialist Party{this.props.party}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Air Pollution</span>
+              <span className='value'>11 Micrograms per cubic metre{this.props.airPollution}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Water Quality</span>
+              <span className='value'>72%{this.props.waterQuality}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Unemployment Rate</span>
+              <span className='value'>7.66{this.props.unemployment}</span>
+            </div>
+          </div>
+          <div className="row">
+            <div className='information-field left'>
+              <span className='label'>Feeling Safe Walking Alone At Night</span>
+              <span className='value'>82.1%{this.props.nightSafety}</span>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default Info;
